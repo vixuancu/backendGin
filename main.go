bb2cc7cfea01c1f640e11cd3f119a7ea5bc0d9c2
vixuancu <vixuancu2004@gmail.com>
2025-06-10 14:27:05 +0700

    URL, Path, Path Param & Query Param

--- main.go
+++ main.go
@@ -1,38 +1,40 @@
 package main
 
-import (
-	"encoding/json"
-	"log"
-	"net/http"
-)
+import "github.com/gin-gonic/gin"
 
 func main() {
-	// This is a placeholder for the main function.
-	// You can add your code here to run the application.
-	http.HandleFunc("/demo", demoFunc)
-	log.Println("hello world")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatal("Failed to start server:", err)
-	}
-}
-func demoFunc(w http.ResponseWriter, req *http.Request) {
-	log.Printf("%+v", req)
-	if req.Method != http.MethodGet {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	respone := map[string]string{
-		"message": "Lap trinh golang",
-		"info":    "vixuancu",
-	}
-	w.Header().Set("Content-Type", "application/json") //set giá trị trả về là "application/json"
-	w.Header().Set("X-Courese", "Lap trinh Golang")    //set giá trị của header X-Courese
-	//data, err := json.Marshal(respone)
-	//if err != nil {
-	//	http.Error(w, "Internal server error lỗi", http.StatusInternalServerError)
-	//	return
-	//}
-	//w.Write(data)
-	json.NewEncoder(w).Encode(respone) // Sử dụng json.NewEncoder để tự động mã hóa và ghi vào ResponseWriter
+
+	r := gin.Default()
+	r.GET("/demo", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.GET("/users", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"data": []string{"Alice", "Bob", "Charlie"},
+		})
+	})
+	r.GET("/users/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		c.JSON(200, gin.H{
+			"id":   id,
+			"data": []string{"Alice", "Bob", "Charlie"},
+		})
+	})
+	r.GET("/products", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"data": []string{"Laptop", "Smartphone", "Tablet"},
+		})
+	})
+	r.GET("/products/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		price := c.Query("price")
+		c.JSON(200, gin.H{
+			"id":    id,
+			"price": price,
+			"data":  []string{"Laptop", "Smartphone", "Tablet"},
+		})
+	})
+	r.Run(":8080") // listen and serve on
 }
